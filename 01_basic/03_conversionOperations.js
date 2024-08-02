9358de2b286908d153cfe4ebbe6015532b9f0e97
sumitgandhi1994 <85626910+sumitgandhi1994@users.noreply.github.com>
2024-08-02 09:16:42 +0000

    Conversion of DataTypes

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,30 @@
+// let score = null;
+// let score4 = 33;
+// let score1 = undefined;
+// let score2 = "33abc";
+// let score3 = "33";
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// let valueInNumber1 = Number(score1);
+// let valueInNumber2 = Number(score2);
+// let valueInNumber3 = Number(score3);
+// let valueInNumber4 = Number(score4);
+// // console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(valueInNumber1);
+// console.log(valueInNumber2);
+// console.log(valueInNumber3);
+// console.log(valueInNumber4);
+
+let isLoggedIn = undefined;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);

92152e1e6498f6160628f4ef0149531d391669ef
sumitgandhi1994 <85626910+sumitgandhi1994@users.noreply.github.com>
2024-08-08 13:46:00 +0000

    Number and Math Fun LIB

---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,36 @@
+const score = 100;
+// console.log(score);
+// console.log(typeof score);
+
+const Balance = new Number(152100.858648284);
+// console.log(Balance);
+// console.log(typeof Balance);
+// console.log(Balance.toString().length)
+// console.log(Balance.toFixed(2));
+// console.log(Balance.toPrecision(3));
+// console.log(Balance);
+
+const hundreds = 1000000000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+///////////Maths /////////////
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.pow(4, 5));
+// console.log(Math.getBaseLog(2, 625));
+
+const min = 10;
+const max = 20;
+
+// console.log(Math.floor(Math.random()*10));
+const rn = Math.random();
+console.log(rn);
+console.log((max - min + 1));
+console.log(rn * (max - min + 1));
+
+console.log(Math.floor(rn * (max - min + 1)) + 10);

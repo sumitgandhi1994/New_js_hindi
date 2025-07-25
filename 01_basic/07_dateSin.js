20e593de6f39d6e0485840fea9c0d23aa7ddc602
sumitgandhi1994 <85626910+sumitgandhi1994@users.noreply.github.com>
2025-07-25 04:47:38 +0000

    Date lerning now

---
+++ 01_basic/07_dateSin.js
@@ -0,0 +1,21 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+let myDate1 = new Date(2025, 7, 26);
+let myDate2 = new Date(2025, 7, 26, 5, 36, 100);
+
+// console.log(myDate1);
+// console.log(myDate2);
+// console.log(myDate2.toLocaleString());
+
+let myDate3 = new Date('2025-11-05')
+let myDate4 = new Date('07-25-2025')
+console.log(myDate3.toLocaleString());
+console.log(myDate4.toLocaleString());
